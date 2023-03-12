03cddb54a3f494d76fdf3fd447c352899e81dab3
SS <sun@SSs-MacBook-Pro.cg.shawcable.net>
2023-03-12 17:22:09 -0600

    update a6

--- dashboardA6.py
+++ dashboardA6.py
@@ -8,6 +8,8 @@ import plotly.express as px
 import geopandas as gpd
 import shapely.geometry
 import numpy as np
+from plotly.subplots import make_subplots
+
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -26,11 +28,11 @@ def load_data():
 
     return a6_data_speed, a6_data_camera,a6_data_school
 
-# url = "https://raw.githubusercontent.com/serenasun999/dash624/master/dataset/2023-02-08-DATA624-Assignment4-Data.csv"
 a6_data_speed, a6_data_camera,a6_data_school = load_data()
 
 def school_gpd(school):
     school['the_geom'] = gp.GeoSeries.from_wkt(school['the_geom']) # resign it to mention it is a geometry file
+    school["Quadrant"] = school["ADDRESS"].str[-2:]
     school = school.set_geometry('the_geom') 
     return school
 
@@ -99,45 +101,65 @@ def drawMap(lats, lons, names, school_data, camera_data):
 
     return fig
 
+def barfig(school_data, camera_data):
+    school = school_data.groupby(["Quadrant","GRADES"]).count().reset_index()
+    camera = camera_data.groupby("Quadrant").count().reset_index()
+    fig = make_subplots(rows=1, cols=2)
+
+    fig.add_trace(go.Bar(x=school["Quadrant"], y=school["NAME"], text=school["GRADES"],
+                        base='overlay', name="Schools"),1,1)
+
+    fig.add_trace(go.Bar(x=camera["Quadrant"], y=camera["Point"], name="Cameras"),1,2)
+
+    fig.update_xaxes(title_text="Quadrant", row=1, col=1)
+    fig.update_yaxes(title_text="Number of Schools", row=1, col=1)
+    fig.update_xaxes(title_text="Quadrant", row=1, col=2)
+    fig.update_yaxes(title_text="Number of Cameras", row=1, col=2)
+
+    # update the layout to display the subplots side by side
+    fig.update_layout(title='Distribution of Schools and Cameras in Calgary')
+
+    return fig
+
+
+
 school_data = school_gpd(a6_data_school)
 speed_data = speed_gpd(a6_data_speed)
 camera_data = camera_gpd(a6_data_camera)
 lats, lons, names = convertMultiLine(speed_data)
 fig = drawMap(lats,lons,names,school_data,camera_data)
+fig_1 = barfig(school_data,camera_data)
 
 app.layout = html.Div(
     [
         html.H1("Assignment 6"),
         """
-        Let's explore the sepsis data!
-        Not the best styling so hopefully you can improve it.
-        The code shows some parameters you can manipulate, but there are lots more to try!
+        This assignment will look at three datasets (School location, Traffic camera, and speed limit map) from the city of Calgary. We are trying to proportion of camera installed around school area, and how speed limit designed around school area, and anywhere cameras are installed.
+
+        - School Locations: https://data.calgary.ca/Services-and-Amenities/School-Locations/fd9t-tdn2 \n
+
+        - Traffic Camera: https://data.calgary.ca/Transportation-Transit/Traffic-Cameras/k7p9-kppz/explore \n
+
+        - Speed Limit Map: https://data.calgary.ca/Health-and-Safety/Speed-Limits-Map/rbfp-3tic \n
         """,
+        html.H2("Distribution of Schools and Cameras in Calgary"),
         dcc.Graph(
-            figure=school_data.plot(),
-            style={
-                "width": "80%",
-                "height": "70vh",
-            },
-            id="OurFirstFigure_score",
-        ),
-        dcc.Graph(
-            figure=speed_data.plot(),
-            style={
-                "width": "80%",
-                "height": "70vh",
-            },
-            id="OurFirstFigure",
-        ),
-        dcc.Graph(
-            figure=camera_data.plot(),
+            figure=fig_1,
             style={
                 "width": "80%",
                 "height": "70vh",
             },
             id="OurSecondFigure_score",
-
         ),
+        """
+        Based on the quadrant, there are over 60 schools located in NW, and in SE and SW over 50 schools are located in these two area. Relatively fewer shcools are located in NE area. Most Elementary schools are distributed equally across Calgary but in NW has greater number of Elementary and Senior High schools.
+
+        Based on the quadrant, most cameras are installed in SE since downtown area might be included in and usually downtown has the most cameras installed and has the most traffic in a city. In other three areas (NE,NW,SW) has similar number of cameras installed.
+
+        Initially, the question I would like to discover is that whether there is an asscociation between number of schools and number of cameras in Calgary. After comparing two bargraphs, the result shows the number of schools is not associated with the number of cameras installed in Calgary.
+        The next step is to discover relationship between distribution of cameras installed, speed limitation and schools in Calgary.
+        """,
+        html.H2("Distribution of Schools, Cameras, and Speed Limit in Calgary on Map"),
         dcc.Graph(
             figure=fig,
             style={
@@ -145,7 +167,13 @@ app.layout = html.Div(
                 "height": "70vh",
             },
             id="OurSecondFigure",
-        )
+        ),
+        """
+        As a result, we could not conclude that there is an association between number of schools, number of cameras and speed limit. The area has more schools do not indicate number of installed camera in that area is higher than others. The most cameras are installed on roads in which has speed limit greater than 50km/h,
+        it might because people are more likely to speeding on highways. Generally, schools are loacted close to major roads and cameras are installed in major traffic areas. From the map we could discover, except downtown area, the number of camera installed is equally disributed in Calgary.
+
+        This map can help to decide where to install new cameras based on speed limit, school zones and incident reports. Also base on incident reports, this map can help to design speed limit across Calgary.Furthermore, it can help to resign education resources with community population.
+        """
     ]
 )
 
